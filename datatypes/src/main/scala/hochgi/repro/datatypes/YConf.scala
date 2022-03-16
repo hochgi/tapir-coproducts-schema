8b333e7999cd46acffd4ed75e4c81d519115a5b7
hochgi <giladhoch@gmail.com>
2022-03-16 10:11:12 +0200

    I managed to get the tapir working almost like I wanted with tons of biolerplate. Surely there's a better way…

--- datatypes/src/main/scala/hochgi/repro/datatypes/YConf.scala
+++ datatypes/src/main/scala/hochgi/repro/datatypes/YConf.scala
@@ -1,18 +1,18 @@
 package hochgi.repro.datatypes
 
 sealed trait YConf {
-  def kind: String
+  def kind: YEnum
 }
 object YConf {
   final case class Y1Conf(size: Int) extends YConf {
-    override def kind: String = YEnum.Y1.toString
+    override def kind: YEnum = YEnum.Y1
   }
 
   final case class Y2Conf(name: String) extends YConf {
-    override def kind: String = YEnum.Y2.toString
+    override def kind: YEnum = YEnum.Y2
   }
 
   final case class Y3Conf(depth: Int, model: String) extends YConf {
-    override def kind: String = YEnum.Y3.toString
+    override def kind: YEnum = YEnum.Y3
   }
 }
--- endpoints/src/main/scala/hochgi/repro/endpoints/Base.scala
+++ endpoints/src/main/scala/hochgi/repro/endpoints/Base.scala
@@ -4,9 +4,12 @@ import hochgi.repro.datatypes.YConf.{Y1Conf, Y2Conf, Y3Conf}
 import hochgi.repro.datatypes.YEnum.{Y1, Y2, Y3}
 import hochgi.repro.datatypes._
 import io.circe.generic.auto._
-import io.circe.{Decoder, DecodingFailure, HCursor, parser, Codec => _}
+import io.circe.{HCursor, Json, parser, Codec => _}
 import sttp.model.StatusCode
 import sttp.tapir.Codec.{JsonCodec, PlainCodec}
+import sttp.tapir.Schema.SName
+import sttp.tapir.SchemaType.{SCoproduct, SDiscriminator, SRef, SchemaWithValue}
+import sttp.tapir.generic.Derived
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.{endpoint => ep, _}
@@ -72,20 +75,40 @@ object Base {
         .derived[YConf.Y3Conf]
         .name(Schema.SName("Y3Conf"))
 
-    Schema.oneOfUsingField[YConf, String](_.kind, identity[String])(
-      YEnum.Y1.toString -> y1ConfSchema,
-      YEnum.Y2.toString -> y2ConfSchema,
-      YEnum.Y3.toString -> y3ConfSchema)
+//    // This does not work:
+//    Schema.oneOfUsingField[YConf, YEnum](_.kind, _.toString)(
+//      YEnum.Y1 -> y1ConfSchema,
+//      YEnum.Y2 -> y2ConfSchema,
+//      YEnum.Y3 -> y3ConfSchema).modify(_.kind)(_ => yEnumSchema)
+
+    // Following is an adjusted code from output
+    // when compiling with: TAPIR_LOG_GENERATED_CODE=true
+    // and originally using strings instead of enums.
+    // And it does work.
+    val mappingAsList = List(Y1 -> y1ConfSchema, Y2 -> y2ConfSchema, Y3 -> y3ConfSchema)
+    val discriminator = SDiscriminator(FieldName("kind", "kind"), mappingAsList.view.collect {
+      case (k, Schema(_, Some(fname), _, _, _, _, _, _, _)) => k.toString -> SRef(fname)
+    }.toMap)
+    val sname = SName("hochgi.repro.datatypes.YConf", Nil)
+    val subtypes = mappingAsList.map(_._2)
+    Schema(SCoproduct(subtypes, Some(discriminator)) {
+      case y: Y1Conf => Some(SchemaWithValue(y1ConfSchema.asInstanceOf[Schema[Any]], y.kind))
+      case y: Y2Conf => Some(SchemaWithValue(y2ConfSchema.asInstanceOf[Schema[Any]], y.kind))
+      case y: Y3Conf => Some(SchemaWithValue(y3ConfSchema.asInstanceOf[Schema[Any]], y.kind))
+    }, Some(sname))
   }
 
-  private[this] def decodeYConf(c: HCursor, kind: String): Decoder.Result[YConf] = YEnum.unapply(kind) match {
-    case None => Left(DecodingFailure(s"$kind cannot be inferred as YEnum", Nil))
-    case Some(Y1) => c.downField("size").as[Int].map(Y1Conf.apply)
-    case Some(Y2) => c.downField("name").as[String].map(Y2Conf.apply)
-    case Some(Y3) => for {
-      depth <- c.downField("depth").as[Int]
-      model <- c.downField("model").as[String]
-    } yield Y3Conf(depth, model)
+  private def decodeYConf(c: HCursor, kind: String): DecodeResult[YConf] = yEnumCodec.decode(kind).flatMap { yEnum =>
+    val either = yEnum match {
+      case Y1 => c.downField("size").as[Int].map(Y1Conf.apply)
+      case Y2 => c.downField("name").as[String].map(Y2Conf.apply)
+      case Y3 => for {
+        depth <- c.downField("depth").as[Int]
+        model <- c.downField("model").as[String]
+      } yield Y3Conf(depth, model)
+    }
+
+    either.fold[DecodeResult[YConf]](DecodeResult.Error(c.value.noSpaces, _), DecodeResult.Value.apply)
   }
 
   implicit val yConfCodec: JsonCodec[YConf] = Codec.json[YConf] { s =>
@@ -93,23 +116,76 @@ object Base {
       j <- parser.parse(s)
       c  = j.hcursor
       k <- c.downField("kind").as[String]
-      y <- decodeYConf(c, k)
-    } yield y
+    } yield decodeYConf(c, k)
 
-    either.fold(DecodeResult.Error(s, _), DecodeResult.Value.apply)
+    either.fold(DecodeResult.Error(s, _), identity)
   }{
-    case Y1Conf(size) => "{\"kind\":\"Y1\",\"size\":" + size + "}"
-    case Y2Conf(name) => "{\"kind\":\"Y2\",\"name\":\"" + name + "\"}"
-    case Y3Conf(d, m) => "{\"kind\":\"Y3\",\"depth\":" + d + ",\"model\":\"" + m + "\"}"
-  }
+    case Y1Conf(size) => "{\"kind\":\"" + yEnumCodec.encode(Y1) + "\",\"size\":" + size + "}"
+    case Y2Conf(name) => "{\"kind\":\"" + yEnumCodec.encode(Y2) + "\",\"name\":\"" + name + "\"}"
+    case Y3Conf(d, m) => "{\"kind\":\"" + yEnumCodec.encode(Y3) + "\",\"depth\":" + d + ",\"model\":\"" + m + "\"}"
+  }(yConfSchema)
 
   implicit val listOfYConfSchema: Schema[List[YConf]] = yConfSchema.asIterable[List]
 
-  implicit val xConfSchema: Schema[XConf] = Schema
-    .derived[XConf]
-    .modify(_.kind)(_.copy(schemaType = xEnumSchema.schemaType))
-    .modify(_.ys.each)(_.copy(schemaType = yConfSchema.schemaType))
+  implicit val listOfYConfCodec: Codec[List[String], List[YConf], CodecFormat.Json] = Codec.list(yConfCodec)
 
-  val getX: Endpoint[Unit, Int, ManagedError, XConf, Any] =
-    api.in(path[Int].validate(Validator.inRange(1, 3))).out(jsonBody[XConf])
+  implicit val xConfSchema: Schema[XConf] = implicitly[Derived[Schema[XConf]]]
+    .value
+    .modify(_.kind)(_.copy(
+      schemaType     = xEnumSchema.schemaType,
+      name           = xEnumSchema.name,
+      isOptional     = xEnumSchema.isOptional,
+      description    = xEnumSchema.description,
+      default        = xEnumSchema.default,
+      format         = xEnumSchema.format,
+      encodedExample = xEnumSchema.encodedExample,
+      deprecated     = xEnumSchema.deprecated,
+      validator      = xEnumSchema.validator)
+    )
+    .modify(_.ys.each)(_.copy(
+      schemaType     = yConfSchema.schemaType,
+      name           = yConfSchema.name,
+      isOptional     = yConfSchema.isOptional,
+      description    = yConfSchema.description,
+      default        = yConfSchema.default,
+      format         = yConfSchema.format,
+      encodedExample = yConfSchema.encodedExample,
+      deprecated     = yConfSchema.deprecated,
+      validator      = yConfSchema.validator)
+    )
+
+  implicit val xConfCodec: Codec[String, XConf, CodecFormat.Json] = Codec.json[XConf]{ s =>
+    val either = for {
+      j <- parser.parse(s)
+      c  = j.hcursor
+      k <- c.downField("kind").as[String]
+      a <- c.downField("ys").as[List[Json]].map(_.map(_.noSpaces))
+    } yield for {
+      kind <- xEnumCodec.decode(k)
+      ys   <- listOfYConfCodec.decode(a)
+    } yield XConf(kind, ys)
+
+    either.fold(DecodeResult.Error(s, _), identity)
+  }{ case XConf(kind, ys) =>
+    val sb = new StringBuilder
+    sb ++= "{\"kind\":\""
+    sb ++= xEnumCodec.encode(kind)
+    if (ys.isEmpty) sb ++= "\"}"
+    else {
+      sb ++= "\",\"ys\":"
+      var ch = '['
+      listOfYConfCodec.encode(ys).foreach { yConfStr =>
+        sb += ch
+        ch = ','
+        sb ++= yConfStr
+      }
+      sb ++= "]}"
+    }
+    sb.result()
+  }
+
+  val getX: Endpoint[Unit, Int, ManagedError, XConf, Any] = api
+    .get
+    .in(path[Int].validate(Validator.inRange(1, 3)))
+    .out(anyFromUtf8StringBody[XConf, CodecFormat.Json](xConfCodec))
 }
